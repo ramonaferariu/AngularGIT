424cf394d83ab8212e3437bc9f28fa0dddf0671b
ramonaferariu <ramona.ferariu@endava.com>
2021-12-02 16:50:49 +0200

    v2

--- fs-asignment3/src/app/users/search-users.service.ts
+++ fs-asignment3/src/app/users/search-users.service.ts
@@ -1,5 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable } from 'rxjs';
+import { normalizeGenFileSuffix } from '@angular/compiler/src/aot/util';
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
+  date: any;
+}
+
+export class User{
+  public id:number;
+  public name:string;
+  public username: string;
+  public email:string;
+  public address:string;
+
+  constructor(id:number,name:string, username:string, email:string, address:string){
+    this.id=id;
+    this.name=name;
+    this.username=username;
+    this.address=address;
+    this.email=email;
+  }
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +33,26 @@ export class SearchUsersService {
 
   constructor(private http:HttpClient) { }
 
-  getConfig(){
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+
+  configUrl = 'https://jsonplaceholder.typicode.com/users?name=Leanne%20Graham';
+
+  getUsers():Observable<Config>{
+    return this.http.get<Config>(this.configUrl )
+    .pipe(
+     // map( users => {
+       // return users;
+    //  });
+     // catchError(this.handleError)
+    );
+  }
+
+
+  private handleError(error: HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){
+      console.log('An error occured: ',error.error.message);
+    }else{
+      console.error(`Backend returned code ${error.status},` +
+      `body was: ${error.error}`);
+    }
   }
 }
--- fs-asignment3/src/app/users/users.component.ts
+++ fs-asignment3/src/app/users/users.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { SearchUsersService } from './search-users.service';
-
+import { Config, SearchUsersService } from './search-users.service';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit,OnDestroy {
 
+  config: Config | undefined;
   searchForm : FormGroup;
   searchInput = new FormControl("",[Validators.minLength(3),Validators.required]);
+  searchedUsers = [];
 
-  constructor(fb:FormBuilder,private searchService:SearchUsersService) { 
+  constructor(private searchService:SearchUsersService,private http:HttpClient) { 
     this.searchForm = new FormGroup({
       "searchInput" : this.searchInput
     });
@@ -22,9 +25,44 @@ export class UsersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnDestroy():void{
+
+  }
+
   onSearch(){
-    const name = this.searchForm.controls["name"].value;
-    
+    const name = this.searchForm.controls["searchInput"].value;
+    console.log("input: " + name);
+    this.showConfig();
+   
+  }
+
+
+  showConfig() {
+   /* this.searchService.getUsers()
+      .subscribe((data: Config) => this.config = {
+          heroesUrl: data.heroesUrl,
+          textfile:  data.textfile,
+          date: data.date,
+      });
+
+      console.log(this.config);*/
+
+      this.http.get('https://jsonplaceholder.typicode.com/users?name=Leanne%20Graham')
+      .pipe(map(responseData =>{
+        const users = [];
+        for(const key in responseData){
+         // if(responseData.hasOwnProperty(key)) {
+             users.push({ ...responseData});
+        //}
+        }
+        return users;
+      }))
+
+      .subscribe(users => {
+        //  console.log(users);
+         this.searchedUsers = users;
+         console.log( this.searchedUsers);
+      });
   }
 
 }
